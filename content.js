8d2fda1665d3b192bae3dfa567d8db7153c91865
Shivam <shivamshinde883@gmail.com>
2025-04-23 01:15:56 +0530

    add: implement h1 element check with timeout in window.onload

--- content.js
+++ content.js
@@ -1,5 +1,26 @@
 window.onload = function () {
   console.log("*************** Content script loaded ***************.");
+
+  // wait for h1 to be loaded add delay that cjheck the h1 tag every 2 seconds for 30 seconds
+  let count = 0;
+  const interval = setInterval(() => {
+    const h1 = document.querySelector("h1");
+    if (h1) {
+      clearInterval(interval);
+      startScrapig();
+    } else {
+      count++;
+      if (count > 15) {
+        clearInterval(interval);
+        console.log(
+          "*************** Page did not have h1 in 30s ***************."
+        );
+      }
+    }
+  }, 2000);
+};
+
+const startScrapig = () => {
   // Listen for messages from the popup script
   const h1 = document.querySelector("h1");
   if (h1) {
@@ -9,10 +30,13 @@ window.onload = function () {
       console.log(
         "*************** h1 text: " + h1.innerText + " ***************."
       );
-    }else{
-      console.log("*************** h1 does not contain 'Incident' ***************.");
-      console.log("*************** h1 text: " + h1.innerText + " ***************.");
-      
+    } else {
+      console.log(
+        "*************** h1 does not contain 'Incident' ***************."
+      );
+      console.log(
+        "*************** h1 text: " + h1.innerText + " ***************."
+      );
     }
   } else {
     console.log("*************** No h1 found ***************.");
