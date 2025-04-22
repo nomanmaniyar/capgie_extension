e8ccd39229fa605e2b5bee2924269fac3d307efb
Shivam <shivamshinde883@gmail.com>
2025-04-23 01:11:30 +0530

    add: enhance window.onload to check for h1 element and log its content

--- content.js
+++ content.js
@@ -1,6 +1,20 @@
-window.onload = function() {
-  console.log('*************** Content script loaded ***************.');
-  // Listen for messages from the popup script  
-
-
-}
+window.onload = function () {
+  console.log("*************** Content script loaded ***************.");
+  // Listen for messages from the popup script
+  const h1 = document.querySelector("h1");
+  if (h1) {
+    // check if h1 has "Incident" in it
+    if (h1.innerText.includes("Incident")) {
+      console.log("*************** h1 found ***************.");
+      console.log(
+        "*************** h1 text: " + h1.innerText + " ***************."
+      );
+    }else{
+      console.log("*************** h1 does not contain 'Incident' ***************.");
+      console.log("*************** h1 text: " + h1.innerText + " ***************.");
+      
+    }
+  } else {
+    console.log("*************** No h1 found ***************.");
+  }
+};
